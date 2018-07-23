8a75cf2ab87f00ecf7c243459193b7fb2c086678
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-07-23 23:53:24 +0000

    update review to use sqlalchemy

--- models/review.py
+++ models/review.py
@@ -3,13 +3,15 @@
     Implementation of the Review class
 '''
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     '''
         Implementation for the Review.
     '''
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    text = Column(String(1024), nullable=False)
