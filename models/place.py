63a7d9ac6591a83fdaa32627ee0f58f8ee44399b
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-07-24 00:23:44 +0000

    update amenities and place to sqlalchemy

--- models/place.py
+++ models/place.py
@@ -3,8 +3,24 @@
     Define the class Place.
 '''
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
+
+place_amenity = Table(
+    "place_amenity",
+    Base.metadata,
+    Column(
+        'place_id',
+        String(60),
+        ForeignKey("places.id"),
+        primary_key=True,
+        nullable=False),
+    Column(
+        'amenity_id',
+        String(60),
+        ForeignKey('amenities.id'),
+        primary_key=True,
+        nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -26,9 +42,24 @@ class Place(BaseModel, Base):
         "Review",
         backref='place',
         cascade='all, delete-orphan')
+    amenities = relationship(
+        "Aemnity", secondary='place_amenity', viewonly=False)
+    amenity_ids = []
 
     @property
     def reviews(self):
         ''' Getters for properties'''
         get_all = models.storage.all('Reviews')
         return [obj for obj in get_all if obj.place_id == self.id]
+
+    @property
+    def amenities(self):
+        ''' Getters for amenities'''
+        get_all = models.storage.all('Amenities')
+        return [obj for obj in get_all if obj.amenity_ids == Amenity.id]
+
+    @amenities.setter
+    def amenities(self, obj):
+        ''' Setter for amenities'''
+        if isinstance(obj, 'Amenities'):
+            self.amenity_ids.append(obj.id)
