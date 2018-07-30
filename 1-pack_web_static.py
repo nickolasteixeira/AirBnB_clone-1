a1afa3454b65e4ec94904b5b20de269100d36bf3
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-07-30 20:38:59 +0000

    print out file complete

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 from fabric.api import local, put, env
 from datetime import datetime
+from os import stat
 
 env.hosts = ['localhost']
+
 
 def do_pack():
     n = datetime.now()
     filename = "web_static_{}{}{}{}{}{}.tgz".format(n.year, n.month, n.day,
                                                     n.hour, n.minute, n.second)
-    path = local("tar -cvzf {} web_static".format(filename))
     local("mkdir -p versions")
-    put("{}".format(filename), "~/AirBnB_clone_v2/versions")
-
-    return path
+    local("tar -cvzf versions/{} web_static".format(filename))
+    print("web_static packed: versions/{} -> {}".format(filename,
+                                                        stat('versions/' + filename).st_size))
