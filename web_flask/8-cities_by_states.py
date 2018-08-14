aad0615a15a7dd19d15025e0cc7fe0575f79c040
Nickolas Teixeira <nickolasteixeira01@gmail.com>
2018-08-14 19:49:15 +0000

    check for filestorage

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import render_template
 from models import storage
 from models import State
+from os import getenv
 '''Basic Flask Application'''
 
 
@@ -13,14 +14,20 @@ app.url_map.strict_slashes = False
 @app.route('/states_list')
 def states_list():
     '''list all states'''
-    states = storage.all(State).values()
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        states = storage.all(State).values()
+    else:
+        states = storage.all(State).values()
     return render_template('7-states_list.html', states=states)
 
 
 @app.route('/cities_by_states')
 def cities_list():
     '''list all states and cities'''
-    states = storage.all(State).values()
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        states = storage.all(State).values()
+    else:
+        states = storage.all(State).values()
     return render_template('8-cities_by_states.html', states=states)
 
 
